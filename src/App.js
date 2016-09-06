964d50164bc6a6d93854f530576094dc19cf0a37
Albert Kawmi <albert.kawmi@gmail.com>
2016-09-06 17:44:56 +0100

    first attempt

---
+++ src/App.js
@@ -0,0 +1,153 @@
+import React, { Component } from 'react';
+import Dragula from 'react-dragula';
+import ReactDOM from 'react-dom';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lists: {
+        hash0: [
+          'hash1',
+          'hash2',
+          'hash3',
+        ],
+        hash4: [
+          'hash5',
+          'hash6',
+          'hash7',
+        ]
+      },
+      items: {
+        hash1: <ToggleItem>Item for hash 1</ToggleItem>,
+        hash2: <ToggleItem>Item for hash 2</ToggleItem>,
+        hash3: <ToggleItem>Item for hash 3</ToggleItem>,
+        hash5: <ToggleItem>Item for hash 5</ToggleItem>,
+        hash6: <ToggleItem>Item for hash 6</ToggleItem>,
+        hash7: <ToggleItem>Item for hash 7</ToggleItem>
+      }
+    };
+    this.initDragula = this.initDragula.bind(this);
+  }
+  componentWillMount() {
+    setInterval(() => {
+      this.setState({
+        items: {
+          ...this.state.items,
+          hash2: <ToggleItem>Random item {Math.floor(Math.random() * 10)}</ToggleItem>
+        }
+      });
+    }, 5000);
+    this.drake = Dragula([], {
+      accepts() {
+        return true;
+      }
+    });
+    this.drake.on('drop', (el, target, source) => {
+      if (!target) return;
+      const targetId = target.getAttribute('data-id');
+      const newTarget = Array.from(target.children)
+          .map(child => child.getAttribute('data-id'));
+      const updatedLists = {
+        [targetId]: newTarget
+      };
+      if (source) {
+        const sourceId = source.getAttribute('data-id');
+        const newSource = Array.from(source.children)
+          .map(child => child.getAttribute('data-id'));
+        updatedLists[sourceId] = newSource;
+      }
+      this.setState({
+        lists: {
+          ...this.state.lists,
+          ...updatedLists
+        }
+      });
+    });
+  }
+  componentWillUpdate(nextProps, nextState) {
+    console.debug(nextState);
+  }
+  componentWillUnmount() {
+    this.drake.destroy();
+  }
+  initDragula(container) {
+    if (container) {
+      this.drake.containers.push(container);
+    }
+  }
+  render() {
+    return (
+      <div className="container">
+        {Object.keys(this.state.lists).map(listId =>
+          <DragAndDropList
+            key={listId}
+            listId={listId}
+            init={this.initDragula}
+            list={this.state.lists[listId]}
+            items={this.state.items}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+class ToggleItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false
+    }
+    this.toggle = this.toggle.bind(this);
+  }
+  toggle() {
+    this.setState({ active: !this.state.active})
+  }
+  render() {
+    return (
+      <span style={{
+        color: this.state.active
+          ? 'green'
+          : 'black'
+      }}>
+        <input type="checkbox" onChange={this.toggle}/>
+        {this.props.children}
+      </span>
+    );
+  }
+}
+
+class DragAndDropList extends Component {
+  shouldComponentUpdate() {
+    return false;
+  }
+  rootRef(rootEl) {
+    if (rootEl) {
+      this.rootEl = rootEl;
+      this.props.init(rootEl);
+    }
+  }
+  renderRows() {
+    if (this.rootEl) {
+      const { list, items, listId } = this.props;
+      ReactDOM.render(
+        this.rootEl
+      );
+    }
+  }
+  componentDidMount() {
+    this.renderRows();
+  }
+  componentDidUpdate() {
+    this.renderRows();
+  }
+  render() {
+    const { list, listId, items } = this.props;
+    return (
+      <ul className="dnd" ref={el => this.rootRef(el)} data-id={listId}>
+        {list.map(id => <li key={id} data-id={id}>{items[id]}</li>)}
+      </ul>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import '../node_modules/react-dragula/dist/dragula.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
