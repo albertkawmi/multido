f360e64f9fe313ebbb3fb9a290074d426ee5f7e5
Albert Kawmi <albert.kawmi@gmail.com>
2017-04-13 23:57:08 +0100

    completed refactor

--- src/App.js
+++ src/App.js
@@ -1,87 +1,169 @@
-import React, { Component } from 'react';
-import initialState from './data';
+import React, { Component, PropTypes } from 'react';
+import initialState from '../data/sample.json';
 import { dndContainer, dndElement } from './dragDrop';
+import bindActions from './actions';
 
-export default class App extends Component {
+export default class Boards extends Component {
   constructor(props) {
     super(props);
     this.state = initialState;
+    this.actions = bindActions(this);
+  }
+  getChildContext() {
+    return {
+      actions: this.actions,
+    };
   }
   componentWillUpdate(nextProps, nextState) {
     console.log(nextState);
   }
-  handleDrop(parentKey, childKey) {
-    return ({ source, target }) => this.setState({
-      [parentKey]: {
-        ...this.state[parentKey],
-        [source.id]: {
-          ...this.state[parentKey][source.id],
-          [childKey]: source.elements
-        },
-        [target.id]: {
-          ...this.state[parentKey][target.id],
-          [childKey]: target.elements
-        }
-      }
-    });
-  }
   render() {
-    const { boards, lists, items } = this.state;
-    const { board1: board } = boards;
+    const { boards } = this.state;
     return (
-      <DndBoard onDrop={this.handleDrop('boards', 'lists')}>
-        {board.lists.map(listId => {
-          const list = lists[listId];
-          return list && <DraggableList {...list} key={list.id} onDrop={this.handleDrop('lists', 'items')}>
-            {list.items.map(itemId => {
-              const item = items[itemId];
-              return (
-                item && <DraggableListItem {...item} key={item.id} />
-              );
-            })}
-          </DraggableList>
-        })}
-      </DndBoard>
+      <main className="boards">
+        {Object.keys(boards).map(
+          boardId => <Board {...boards[boardId]} key={boardId} />
+        )}
+        <button
+          className="new-board-btn"
+          onClick={this.actions.addNewBoard}>
+          + New Board
+        </button>
+      </main>
     );
   }
 }
 
-const Board = ({ children }) => <div className="board">{children}</div>;
+/*******************************************************************************
+ * Board
+ */
+const ContainerBoard = ({ children }) => <section className="board__items">{children}</section>;
 
 const DndBoard = dndContainer({
   containerType: 'board',
   acceptType: 'list',
-  handleClassName: 'list__handle',
-  direction: 'vertical'
-})(Board);
+  handleClassName: 'list__title',
+  direction: 'horizontal'
+})(ContainerBoard);
 
-const List = ({ children }) => <ul className="list">{children}</ul>;
+const Board = ({ title, lists: listIds, id: boardId }, { actions }) => {
+  const state = actions.getState();
+  const onListDrop = actions.handleDrop('boards', 'lists');
+  const lists = listIds.map(listId => state.lists[listId]);
+  return (
+    <div className="board">
+      <input
+        className="board__title"
+        value={title}
+        onChange={ev => actions.update.boards(boardId, {
+          title: ev.target.value
+        })}
+        placeholder="(untitled)"
+      />
+      <DndBoard id={boardId} onDrop={onListDrop}>
+        {lists.map(
+          list => <DraggableList {...list} key={list.id} />
+        )}
+      </DndBoard>
+      <button
+        className="board__new-list"
+        onClick={() => actions.addListToBoard(boardId)} >
+        + New List
+      </button>
+    </div>
+  );
+}
 
-const ContainerList = dndContainer({
+/*******************************************************************************
+ * List
+ */
+const Items = ({ children }) => <ul className="list__items">{children}</ul>;
+
+const DropItems = dndContainer({
   containerType: 'list',
   acceptType: 'item',
   handleClassName: 'item__handle',
   direction: 'vertical'
-})(List);
+})(Items);
 
-const ListWithHandle = props => (
-  <div>
-    <div className="list__handle" />
-    <ContainerList {...props} />
-  </div>
-);
+const List = ({ id: listId, title, items: itemIds }, { actions }) => {
+  const state = actions.getState();
+  const items = itemIds.map(itemId => state.items[itemId]);
+  const onItemDrop = actions.handleDrop('lists', 'items');
+  return (
+    <div className="list">
+      <input
+        className="list__title"
+        onChange={ev => actions.update.lists(
+          listId,
+          { title: ev.target.value }
+        )}
+        value={title}
+        placeholder="(untitled)"
+      />
+      <DropItems id={listId} onDrop={onItemDrop} >
+        {items.map(
+          item => <DraggableItem {...item} key={item.id} />
+        )}
+      </DropItems>
+      <button
+        className="list__add-item"
+        onClick={() => actions.addItemToList(listId)}>
+        + Add an item
+      </button>
+    </div>
+  );
+}
 
 const DraggableList = dndElement({
   type: 'list',
-})(ListWithHandle);
+})(List);
 
-const Item = ({ text }) => (
+/*******************************************************************************
+ * Item
+ */
+const itemClassname = completed =>
+  `item__textarea ${completed ? 'item__textarea--completed' : ''}`;
+
+const itemTextHeight = text => ({
+  height: `${Math.ceil(
+    text.replace('\n', '').length / 27 +
+    1.1 * text.split('\n').length - 1 + 0.25)
+  }rem`
+});
+
+const Item = ({ id, text, completed }, { actions }) => (
   <li className="item">
     <span className="item__handle" />
-    {text}
+    <textarea
+      className={itemClassname(completed) + ' dynamic-textarea'}
+      style={itemTextHeight(text)}
+      onChange={ev => actions.update.items(id, { text: ev.target.value })}
+      value={text}
+      placeholder="(empty)"
+    />
+    <input
+      className="item__checkbox"
+      id={id}
+      type="checkbox"
+      checked={completed}
+      onChange={() => actions.update.items(id, { completed: !completed })}
+      />
   </li>
 );
 
-const DraggableListItem = dndElement({
+const DraggableItem = dndElement({
   type: 'item'
 })(Item);
+
+/*******************************************************************************
+ * Context boilerplate
+ */
+const CONTEXT = {
+  actions: PropTypes.object
+}
+
+Boards.childContextTypes = CONTEXT;
+Board.contextTypes = CONTEXT;
+List.contextTypes = CONTEXT;
+Item.contextTypes = CONTEXT;
---
+++ src/dragDrop/index.js
@@ -0,0 +1,135 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Dragula from 'react-dragula';
+
+const DRAGGABLE_TYPE = 'dnd-draggable-type';
+const DRAGGABLE_ID = 'dnd-draggable-id';
+const CONTAINER_TYPE = 'dnd-container-type';
+const CONTAINER_ID = 'dnd-container-id';
+
+const dndStore = {};
+
+export const dndContainer = ({
+  idProp = 'id',
+  containerType,
+  acceptType,
+  handleClassName,
+  direction = 'vertical'
+}) => (ComponentToWrap) => {
+  const WrappedComponent = ensureClassComponent(ComponentToWrap);
+
+  if (!dndStore[containerType]) {
+    dndStore[containerType] = Dragula([], {
+      accepts(el, target) {
+        return el.getAttribute(DRAGGABLE_TYPE) === acceptType
+          && target.getAttribute(CONTAINER_TYPE) === containerType;
+      },
+      moves(el, source, handle) {
+        if (!handleClassName) return true;
+        return handle.classList.contains(handleClassName);
+      },
+      direction
+    });
+  }
+  return class extends Component {
+    storeIndex = 0
+
+    componentWillMount() {
+      dndStore[containerType].on('drop', (el, target, source) => {
+        if (!target || !source) return;
+        // update the list of ids in the target container
+        const targetId = target.getAttribute(CONTAINER_ID);
+        const updatedTargetElements = getAttrValues(DRAGGABLE_ID, target);
+        // update the list of ids in the source container (if different to target)
+        const sourceId = source.getAttribute(CONTAINER_ID);
+        const updatedSourceElements = sourceId === targetId
+          ? updatedTargetElements
+          : getAttrValues(DRAGGABLE_ID, source);
+
+        this.props.onDrop({
+          source: { id: sourceId, elements: updatedSourceElements },
+          target: { id: targetId, elements: updatedTargetElements }
+        });
+      })
+    }
+    componentWillUnmount() {
+      dndStore[containerType].containers.splice(this.storeIndex, 1);
+    }
+    rootRef(component) {
+      const el = ReactDOM.findDOMNode(component);
+      if (!el) return;
+      el.setAttribute(CONTAINER_TYPE, containerType)
+      el.setAttribute(CONTAINER_ID, this.props[idProp]);
+      this.storeIndex = dndStore[containerType].containers.length;
+      dndStore[containerType].containers.push(el);
+    }
+    render() {
+      return (
+        <WrappedComponent
+          {...this.props}
+          ref={this.rootRef.bind(this)}
+          key={`${containerType}-${this.props.children.length}`} />
+      );
+    }
+  };
+}
+
+export const dndElement = ({
+  idProp = 'id',
+  type
+}) => (ComponentToWrap) => {
+  const WrappedComponent = ensureClassComponent(ComponentToWrap);
+
+  return class extends Component {
+    rootRef(component) {
+      const el = ReactDOM.findDOMNode(component);
+      if (!el) return;
+      el.setAttribute(DRAGGABLE_ID, this.props[idProp]);
+      el.setAttribute(DRAGGABLE_TYPE, type);
+    }
+    render() {
+      return (
+        <WrappedComponent {...this.props} ref={this.rootRef.bind(this)}/>
+      );
+    }
+  }
+};
+
+function getAttrValues(attr, el) {
+  // an ID attribute is used to only select direct child elements
+  const originalId = el.getAttribute('id');
+  let tempId;
+  if (!originalId) {
+    tempId = `TEMP_ID_${Date.now()}`
+    el.setAttribute('id', tempId);
+  }
+
+  // find matching elements and map to the required attr
+  const scopeId = originalId || tempId;
+  const matchingAttrs = Array
+    .from(el.querySelectorAll(`#${scopeId} > [${attr}]`))
+    .map(el => el.getAttribute(attr));
+
+  // remove the tempId, if needed
+  if (!originalId) el.removeAttribute('id');
+
+  return matchingAttrs;
+}
+
+/**
+ * This is needed so that we can attach a ref to get the root DOM node
+ * for drag-and-drop purposes
+ */
+function ensureClassComponent(ComponentToWrap) {
+  const isStateless = typeof ComponentToWrap.prototype.render !== 'function';
+
+  return isStateless
+    ? convertToClass(ComponentToWrap)
+    : ComponentToWrap;
+}
+
+function convertToClass(StatelessComponent) {
+  return class extends Component {
+    render = () => <StatelessComponent {...this.props} />;
+  };
+}
