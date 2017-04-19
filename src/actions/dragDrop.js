d940b6044fc622392db609a71858a185a1d74c9b
Albert Kawmi <albert.kawmi@gmail.com>
2017-04-20 00:32:19 +0100

    forgot to commit actions/dragDrop.js

--- src/actions/dragDrop.js
+++ src/actions/dragDrop.js
@@ -3,16 +3,19 @@ import { updateFor } from './crud';
 export function createDropHandler(parentKey, childKey) {
   return ({ source, target }) => (dispatch, getState) => {
     const parent = getState()[parentKey];
+
     const existingSource = parent[source.id];
     const updatedSource = {
       ...existingSource,
       [childKey]: source.elements
     };
+
     const existingTarget = parent[target.id];
     const updatedTarget = {
       ...existingTarget,
       [childKey]: target.elements
     };
+
     const exSourceCompare = existingSource[childKey].toString();
     const updSourceCompare = updatedSource[childKey].toString();
     const exTargetCompare = existingTarget[childKey].toString();
