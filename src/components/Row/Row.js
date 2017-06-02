7d0a77981b0ba27eaf73a8ddb7bf6f60e73f4137
Albert Kawmi <albert.kawmi@gmail.com>
2017-06-03 00:35:40 +0100

    scroll to end of row when new list is added

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { dndContainer } from 'react-dragula-hoc';
 import List from '../List';
 
@@ -11,33 +12,48 @@ const DndRow = dndContainer({
   direction: 'horizontal'
 })(ContainerRow);
 
-const Row = ({
-  row: { id: rowId, title },
-  lists,
-  onListDrop,
-  onTitleChange,
-  onListCreated
-}) => {
-  return (
-    <div className="row">
-      <input
-        className="row__title"
-        value={title}
-        onChange={onTitleChange}
-        placeholder="Untitled Row"
-      />
-      <DndRow id={rowId} onChange={onListDrop}>
-        {lists.map(
-          list => <List list={list} id={list.id} key={list.id} />
-        )}
-      </DndRow>
-      <button
-        className="row__new-list"
-        onClick={onListCreated} >
-        + New List
-      </button>
-    </div>
-  );
+class Row extends Component {
+  componentDidUpdate() {
+    if (this.shouldScroll) {
+      this.rootEl.scrollLeft = this.rootEl.scrollWidth;
+      this.shouldScroll = false;
+    }
+  }
+  onNewListClicked() {
+    this.shouldScroll = true;
+    this.props.onListCreated();
+  }
+  handleRef(component) {
+    this.rootEl = ReactDOM.findDOMNode(component);
+  }
+  render() {
+    const {
+      row: { id: rowId, title },
+      lists,
+      onListDrop,
+      onTitleChange,
+    } = this.props;
+    return (
+      <div className="row">
+        <input
+          className="row__title"
+          value={title}
+          onChange={onTitleChange}
+          placeholder="Untitled Row"
+        />
+        <DndRow ref={this.handleRef.bind(this)} id={rowId} onChange={onListDrop}>
+          {lists.map(
+            list => <List list={list} id={list.id} key={list.id} />
+          )}
+        </DndRow>
+        <button
+          className="row__new-list"
+          onClick={this.onNewListClicked.bind(this)} >
+          + New List
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Row;
